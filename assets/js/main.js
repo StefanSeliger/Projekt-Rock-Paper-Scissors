37be484b9d0b90be28c3e163c74e75f1733b415b
StefanSeliger <Your_seligerstefan84@gmail.com>
2021-12-05 15:35:37 +0100

    fourth

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,28 @@
+let form = document.getElementById("formId")
+
+// Round Selection
 let round5 = document.getElementById("round5")
 let round10 = document.getElementById("round10")
 let round15 = document.getElementById("round15")
 let round20 = document.getElementById("round20")
-
-let rounds = document.getElementsByName("rounds")
+let roundSelection = document.getElementById("roundSelection")
 
 let roundsPlayed = document.getElementById("roundsPlayed")
-let result = document.getElementById("results")
+let result = document.getElementById("result")
 let gameOutput = document.getElementById("gameOutput")
-let roundSelection = document.getElementById("roundSelection")
+
+// User Choices
+let rockId = document.getElementById("rockId")
+let paperId = document.getElementById("paperId")
+let scissorsId = document.getElementById("scissorsId")
 let rock = "rock"
 let paper = "paper"
 let scissors = "scissors"
 
+// Computer Choices
 let compArray = ["rock", "paper", "scissors"]
 
+// Counting Wins & Rounds
 let userWins = document.getElementById("userWins")
 let userPoints = 0
 let compWins = document.getElementById("compWins")
@@ -22,37 +30,39 @@ let compPoints = 0
 let countRound = 0
 
 let userChoice = (x) => {
+    let formRounds = form.rounds.value
     let compArrayRandom = compArray[Math.floor(Math.random() * 3)]
     roundSelection.style.display = "none"
     countRound += 1
-    roundsPlayed.innerHTML = `${countRound} / rounds.value` 
+    roundsPlayed.innerHTML = `${countRound} / ${formRounds}` 
 
     if (compArrayRandom == x) { // tie
-        console.log("tie")
         gameOutput.innerHTML = "It's a tie!"
     } else if (compArrayRandom != x) { // computer wins
         if (compArrayRandom == rock && x == scissors || compArrayRandom == paper && x == rock || compArrayRandom == scissors && x == paper) {
-            console.log("user looses")
             gameOutput.innerHTML = "The computer wins, user looses."
             compPoints +=1
             compWins.innerHTML = compPoints
         } else { // user wins
-            console.log("user wins")
             gameOutput.innerHTML = "The user wins, computer looses."
             userPoints +=1
             userWins.innerHTML = userPoints
         }
     } 
+
+    if (countRound == formRounds) {
+        rockId.disabled = true
+        paperId.disabled = true
+        scissorsId.disabled = true
+        if (compPoints < userPoints) {
+            result.innerHTML = "User Wins"
+        } else if (compPoints > userPoints) {
+            result.innerHTML = "Computer Wins"
+        } else {
+            result.innerHTML = "It's a tie!"
+        }
+    }
 }
 
-let roundCalc = () => {  
-    if (round5.checked) {
-        return 5
-    } else if (round10.checked) {
-        return 10
-    } else if (round15.checked) {
-        return 15
-    } else if (round20.checked) {
-        return 20
-    } 
-}
+
+
