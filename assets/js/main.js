4b8e778497ce485ab34a848e3596861b41efb475
StefanSeliger <Your_seligerstefan84@gmail.com>
2021-12-05 09:56:22 +0100

    second

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+let round5 = document.getElementById("round5")
+let round10 = document.getElementById("round10")
+let round15 = document.getElementById("round15")
+let round20 = document.getElementById("round20")
+let roundsPlayed = document.getElementById("roundsPlayed")
+let results = document.getElementById("results")
+let gameOutput = document.getElementById("gameOutput")
+let rock = document.getElementById("rock")
+let paper = document.getElementById("paper")
+let scissors = document.getElementById("scissors")
+
+rock.addEventListener("click", () => {
+    console.log("hello World")
+})
+
+paper.addEventListener("click", () => {
+    console.log("hello World")
+})
+
+scissors.addEventListener("click", () => {
+    console.log("hello World")
+})
