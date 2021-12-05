463f17ea8671e3df2cad5c0da17f0477e8afde24
StefanSeliger <Your_seligerstefan84@gmail.com>
2021-12-05 13:54:14 +0100

    third

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,57 @@ let round5 = document.getElementById("round5")
 let round10 = document.getElementById("round10")
 let round15 = document.getElementById("round15")
 let round20 = document.getElementById("round20")
+
+let rounds = document.getElementsByName("rounds")
+
 let roundsPlayed = document.getElementById("roundsPlayed")
-let results = document.getElementById("results")
+let result = document.getElementById("results")
 let gameOutput = document.getElementById("gameOutput")
-let rock = document.getElementById("rock")
-let paper = document.getElementById("paper")
-let scissors = document.getElementById("scissors")
+let roundSelection = document.getElementById("roundSelection")
+let rock = "rock"
+let paper = "paper"
+let scissors = "scissors"
 
-rock.addEventListener("click", () => {
-    console.log("hello World")
-})
+let compArray = ["rock", "paper", "scissors"]
 
-paper.addEventListener("click", () => {
-    console.log("hello World")
-})
+let userWins = document.getElementById("userWins")
+let userPoints = 0
+let compWins = document.getElementById("compWins")
+let compPoints = 0
+let countRound = 0
 
-scissors.addEventListener("click", () => {
-    console.log("hello World")
-})
+let userChoice = (x) => {
+    let compArrayRandom = compArray[Math.floor(Math.random() * 3)]
+    roundSelection.style.display = "none"
+    countRound += 1
+    roundsPlayed.innerHTML = `${countRound} / rounds.value` 
+
+    if (compArrayRandom == x) { // tie
+        console.log("tie")
+        gameOutput.innerHTML = "It's a tie!"
+    } else if (compArrayRandom != x) { // computer wins
+        if (compArrayRandom == rock && x == scissors || compArrayRandom == paper && x == rock || compArrayRandom == scissors && x == paper) {
+            console.log("user looses")
+            gameOutput.innerHTML = "The computer wins, user looses."
+            compPoints +=1
+            compWins.innerHTML = compPoints
+        } else { // user wins
+            console.log("user wins")
+            gameOutput.innerHTML = "The user wins, computer looses."
+            userPoints +=1
+            userWins.innerHTML = userPoints
+        }
+    } 
+}
+
+let roundCalc = () => {  
+    if (round5.checked) {
+        return 5
+    } else if (round10.checked) {
+        return 10
+    } else if (round15.checked) {
+        return 15
+    } else if (round20.checked) {
+        return 20
+    } 
+}
